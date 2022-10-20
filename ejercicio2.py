875cfd8982f88a4871feed967a4ccd987d0154f0
Gerardobt05 <113972734+Gerardobt05@users.noreply.github.com>
2022-10-20 09:58:54 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,4 +3,15 @@
 Crea una lista e inicializala con 5 cadenas de caracteres leídas por teclado. 
 Copia los elementos de la lista en otra lista pero en orden inverso, y muestra sus elementos por la pantalla.
 '''
+def pedirCadenasCaracteres(n):
+    lst = []
+    for i in range(0, n):
+        cadena = input("Dime una palabra: ")
+        lst.append(cadena)
+    return lst
 
+lst = pedirCadenasCaracteres(5)
+lst2 = lst.copy()
+lst2.reverse()
+print("lst", lst)
+print("lst2", lst2)

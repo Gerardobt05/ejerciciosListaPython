d595224fc1506902eb30e4ae553a693171fe6ed0
Gerardobt05 <113972734+Gerardobt05@users.noreply.github.com>
2022-10-24 12:40:57 +0200

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -3,3 +3,23 @@
 Hacer un programa que inicialice una lista de números con valores aleatorios (10 valores), y 
 posterior ordene los elementos de menor a mayor
 '''
+
+import random
+
+#Función que que devuelve una lista con 10 números aleatorios entre 0 y 10
+def listaAleatoria():
+    lst = []
+    for i in range(0,10):
+        lst.append(random.randint(0,10))
+    return lst
+
+ 
+#Creamos una función que ordene de menor a mayor
+def listaMenoraMayor(lst):
+    lst.sort()
+    return lst
+
+def ejercicio5():
+    lst = listaAleatoria()
+    print(listaMenoraMayor(lst))
+    

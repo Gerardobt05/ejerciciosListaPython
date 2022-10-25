8f6d71664dc8b6bec8be8db6a636e32a3f9d6220
Gerardobt05 <113972734+Gerardobt05@users.noreply.github.com>
2022-10-25 09:56:11 +0200

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -4,3 +4,15 @@ Crea un programa que pida un número al usuario, un número de mes ( por ejemplo
 El programa dirá cuántos días tiene (por ejemplo 30) y el nombre del mes. Debes usar listas. Nota: En una lista podemos guardar listas.
 Para simplificarlo vamos a suponer que febrero tiene 28 días
 '''
+def pedirmes():
+    lstMes = [["Enero" , 31],["Febrero" , 28],["Marzo" , 31],["Abril" , 30],["Mayo" , 31],["Junio" , 30],["Julio" , 31],["Agosto" , 31],["Septiembre" , 30],["Octubre" , 31],["Noviembre" , 30],["Diciembre" , 31]]
+    try:
+        numMes = int(input("Dime un número del 1 al 12: "))
+        if(numMes > 12 or numMes < 1):
+            print("Ese número no vale")
+        else:
+            print(lstMes[numMes-1][0], " tiene ", lstMes[numMes-1][1], " días.")
+        
+            
+    except:
+        print("Tiene que ser un número")
